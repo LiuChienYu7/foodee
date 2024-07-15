12c3cff70292291317d45637a699860c36adad14
LiuChienYu7 <c09010011@gmail.com>
2024-07-15 15:49:00 +0800

    7/15 0349

--- connect_sql/get_data_json.php
+++ connect_sql/get_data_json.php
@@ -2,12 +2,7 @@
     header('Content-Type: application/json');
 
     include("connect.php");
-
-    // 檢查是否設置了查詢條件
-    if (!isset($where_condition)) {
-        die("No query condition specified.");
-    }
-
+    
     // 聯合查詢，使用傳遞的查詢條件
     $sql = "
         SELECT *
@@ -17,8 +12,6 @@
             detail d ON a.r_id = d.r_id
         JOIN 
             review r ON a.r_id = r.r_id
-        WHERE 
-            $where_condition
     ";
     
     $result = $conn->query($sql);
--- map/compare_map.php
+++ map/compare_map.php
@@ -13,11 +13,39 @@
      <title>caompare_map</title>
 </head>
 <body>
-    <div id="map"></div>
+    <div id="map" style="width: 600px; height: 400px;" ></div>
+    <?php
+        //連資料庫
+        $where_condition = "r_rate_value > 3";
+        include("../connect_sql/get_data.php");
 
+    ?>
+    
     <script>
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    <script>
+
+        // 地圖的點應該要是變數 看使用者選到哪一家店 以那家店為中心
+        const map = L.map('map').setView([22.63151, 120.30132], 14);
+
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        
+        // 從JSON文件加載數據並顯示在地圖上
+        fetch('../connect_sql/get_data_json.php')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                data.forEach(function(restaurants) {
+                    var marker = L.marker([restaurants.r_latitude, restaurants.r_longitude]).addTo(map);
+                    // 顯示餐廳名稱
+                    marker.bindPopup("name: " + restaurants.r_name).openPopup();
+                });
+            })
+            .catch(error => console.error('Error loading restaurant data:', error));
+        
+
+    </script>
     
     
 </body>
