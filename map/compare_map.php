6ea3548a497d37ce036edfa82ce8a2febe23a9ed
LiuChienYu7 <c09010011@gmail.com>
2024-07-18 11:18:34 +0800

    0718 try figure out mrt icon

--- map/compare_map.php
+++ map/compare_map.php
@@ -10,21 +10,17 @@
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
-     <title>caompare_map</title>
+     <!-- edgeMarker -->
+    <script src="leaflet_edgeMarker.js"></script> 
+    <title>caompare_map</title>
 </head>
 <body>
     <div id="map" style="width: 600px; height: 400px;" ></div>
-    <?php
-        //連資料庫
-        $where_condition = "r_rate_value > 3";
-        include("../connect_sql/get_data.php");
-
-    ?>
     
     <script>
 
         // 地圖的點應該要是變數 看使用者選到哪一家店 以那家店為中心
-        const map = L.map('map').setView([22.63151, 120.30132], 14);
+        const map = L.map('map').setView([22.631386, 120.301951], 13);
 
         const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -37,14 +33,94 @@
             .then(data => {
                 console.log(data);
                 data.forEach(function(restaurants) {
-                    var marker = L.marker([restaurants.r_latitude, restaurants.r_longitude]).addTo(map);
+                    var marker = L.marker([restaurants.r_latitude, restaurants.r_longitude], {
+                        isRestaurant: true // 設置 isRestaurant 屬性 讓edgeMarker辨別是餐廳
+                    }).addTo(map);
+
                     // 顯示餐廳名稱
                     marker.bindPopup("name: " + restaurants.r_name).openPopup();
                 });
             })
             .catch(error => console.error('Error loading restaurant data:', error));
         
+        //加上捷運輕軌
+        fetch('../connect_sql/get_data_map_json.php')
+            .then(response => response.json())
+            .then(data => {
+                // var LRT_points = []
+                var MRT_O_points = []
+                var MRT_R_points = []
+                var MRT = L.icon({
+                            iconUrl: 'mrt.png',
+                            iconSize:     [25, 25], // size of the icon
+                            // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                            // shadowAnchor: [4, 62],  // the same for the shadow
+                            // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+                });
+                console.log(data);
+                data.forEach(function(transportation) {
+                    if(transportation.id.includes('C')){
+                        // LRT_points.push([transportation.latitude, transportation.longitude])
+                        // L.polyline(LRT_points).addTo(map)
+                        var circle = L.circle([transportation.latitude, transportation.longitude], {
+                            color: 'green',
+                            fillColor: 'green',
+                            fillOpacity: 0.9,
+                            radius: 35
+                        }).addTo(map);
 
+                    }
+                    else if(transportation.id.includes('O')){
+                        MRT_O_points.push([transportation.latitude, transportation.longitude])
+                        L.polyline(MRT_O_points,{
+                            color:'orange',
+                            weight: 2
+                        }).addTo(map);
+                        // L.marker([transportation.latitude, transportation.longitude], {icon: MRT}).addTo(map);
+                        var circle = L.circle([transportation.latitude, transportation.longitude], {
+                            color: 'orange',
+                            fillColor: 'orange',
+                            fillOpacity: 0.9,
+                            radius: 35
+                        }).addTo(map)
+                    }
+                    else{
+                        MRT_R_points.push([transportation.latitude, transportation.longitude])
+                        L.polyline(MRT_R_points,{
+                            color:'red',
+                            weight: 2
+                        }).addTo(map);
+                        
+                        // L.marker([transportation.latitude, transportation.longitude], {icon: MRT}).addTo(map)
+                        var circle = L.circle([transportation.latitude, transportation.longitude], {
+                            color: 'red',
+                            fillColor: '#f03',
+                            fillOpacity: 0.9,
+                            radius: 35
+                        }).addTo(map)
+                    }
+                    
+
+                    circle.bindPopup("name: " + transportation.name).openPopup();
+                });
+            })
+            .catch(error => console.error('Error loading restaurant data:', error));
+
+        // add the EdgeMarker to the map. 箭頭
+        var edgeMarkerLayer = L.edgeMarker({
+            icon: L.icon({ // style markers
+                iconUrl: 'edge_arrow_marker.png',
+                clickable: true,
+                iconSize: [25, 25],
+                iconAnchor: [10, 10]
+            }),
+            rotateIcons: true, // rotate EdgeMarkers depending on their relative position
+            layerGroup: null // you can specify a certain L.layerGroup to create the edge markers from.
+        }).addTo(map);
+
+        // if you want to remove the edge markers
+        // edgeMarkerLayer.destroy();
+            
     </script>
     
     
