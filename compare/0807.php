0d3cb46183bc64a2c3630d3bd966913a3aa16b50
LiuChienYu7 <c09010011@gmail.com>
2024-09-19 01:11:29 +0800

    修改資料庫連結

--- compare/0807.php
+++ compare/0807.php
@@ -363,7 +363,7 @@ if ($link) {
         // 構建SQL查詢
         if (!empty($r_ids)) {
             $ids = implode("','", $r_ids); // 將數組中的ID轉換為SQL字符串格式
-            $sql = "SELECT * FROM detail2 WHERE r_id IN ('$ids')";
+            $sql = "SELECT * FROM detail WHERE r_id IN ('$ids')";
 
             $result = $conn->query($sql);
 
